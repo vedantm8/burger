a4040a892ac5fab2e460d887bced402fec3ce8a0
vedantm8 <vedantm8@gmail.com>
2018-08-25 10:44:12 -0400

    addingGif

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Burger (Eat-Da-Burger) Activity 
 
-<a href="https://giphy.com/gifs/lcxSqyqCS6tNOcL8fs"></a>
+![demo/burgersActivity.gif](name-of-giphy.gif)
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3000;
+var port = 3000 || process.env.PORT ;
 
 var app = express();
 
